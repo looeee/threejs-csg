019984db34d8517da92020371e79434d54c717fa
Lewy Blue <looeee@gmail.com>
2020-05-29 18:13:26 +0700

    added lots of test objects to demo

---
+++ demo/demoA/src/csg-test.js
@@ -0,0 +1,108 @@
+import {
+  BoxBufferGeometry,
+  CylinderBufferGeometry,
+  BufferAttribute,
+  BufferGeometry,
+  PlaneBufferGeometry,
+  SphereBufferGeometry,
+  MeshNormalMaterial,
+} from '../../../node_modules/three/build/three.module.js';
+
+import { CSG } from '../../../build/csg.module.js';
+
+function createGeo(positions) {
+  const geometry = new BufferGeometry();
+
+  geometry.setAttribute(
+    'position',
+    new BufferAttribute(new Float32Array(positions), 3),
+  );
+
+  return geometry;
+}
+
+function testCSG(params) {
+  // prettier-ignore
+  const triangle = createGeo([
+    -0.2, -0.2,  0,
+    0.2, -0.2,  0,
+    0.2,  0.2,  0,
+  ] );
+
+  // prettier-ignore
+  const triangleUp = createGeo([
+    -0.2, -0.2,  0,
+    -0.2, 0.2,  0,
+    0.2,  0.2,  0,
+  ] );
+
+  // prettier-ignore
+  const square = createGeo([
+    -0.2, -0.2,  0,
+    0.2, -0.2,  0,
+    0.2,  0.2,  0,
+
+    0.2,  0.2,  0,
+    -0.2,  0.2,  0,
+    -0.2, -0.2,  0
+  ]);
+
+  const box = new BoxBufferGeometry(0.2, 0.2, 1);
+  const cylinder = new CylinderBufferGeometry(0.2, 0.2, 1);
+  const plane = new PlaneBufferGeometry(0.2, 0.2);
+  const sphere = new SphereBufferGeometry(0.2);
+
+  const triangleCSG = new CSG().fromGeometry(triangle);
+  const triangleUpCSG = new CSG().fromGeometry(triangleUp);
+  const squareCSG = new CSG().fromGeometry(square);
+  const planeCSG = new CSG().fromGeometry(plane);
+  // console.log('planeCSG: ', plane, planeCSG);
+  const boxCSG = new CSG().fromGeometry(box);
+  const boxBCSG = new CSG().fromGeometry(
+    new BoxBufferGeometry(0.3, 0.3, 0.3),
+  );
+  // console.log('boxCSG: ', box, boxCSG);
+  const cylinderCSG = new CSG().fromGeometry(cylinder);
+  // console.log('cylinderCSG: ', cylinder, cylinderCSG);
+  const sphereCSG = new CSG().fromGeometry(sphere);
+  // console.log('sphereCSG: ', sphere, sphereCSG);
+
+  // const basic = triangleCSG.subtract(squareCSG);
+  // const basic = squareCSG.subtract(triangleCSG);
+  const basic = triangleCSG.union(triangleUpCSG);
+
+  console.log(
+    'triangle: ',
+    triangleCSG.toGeometry().attributes.position.array,
+  );
+  console.log(
+    'triangleUp: ',
+    triangleUpCSG.toGeometry().attributes.position.array,
+  );
+  console.log(
+    'basic: ',
+    basic.toGeometry().attributes.position.array,
+  );
+
+  // const subTest = boxCSG.subtract(boxBCSG);
+  // console.log('subTest: ', subTest.toGeometry());
+
+  return {
+    triangle: triangleCSG.toGeometry(),
+    triangleOrig: triangle,
+    square: squareCSG.toGeometry(),
+    squareOrig: square,
+    plane: planeCSG.toGeometry(),
+    planeOrig: plane.toNonIndexed(),
+    box: boxCSG.toGeometry(),
+    boxOrig: box.toNonIndexed(),
+    cylinder: cylinderCSG.toGeometry(),
+    cylinderOrig: cylinder.toNonIndexed(),
+    sphere: sphereCSG.toGeometry(),
+    sphereOrig: sphere.toNonIndexed(),
+    basic: basic.toGeometry(),
+    // subTest: subTest.toGeometry(),
+  };
+}
+
+export { testCSG };
--- demo/demoA/src/main.js
+++ demo/demoA/src/main.js
@@ -1,6 +1,7 @@
 import {
   BoxBufferGeometry,
   Color,
+  DoubleSide,
   Mesh,
   MeshNormalMaterial,
   PerspectiveCamera,
@@ -9,6 +10,8 @@ import {
 } from '../../../node_modules/three/build/three.module.js';
 
 import { OrbitControls } from '../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+
+import { testCSG } from './csg-test.js';
 
 function init() {
   const container = document.querySelector('#scene-container');
@@ -19,16 +22,83 @@ function init() {
     0.1,
     100,
   );
-  camera.position.z = 1;
+  camera.position.z = 5;
 
   const scene = new Scene();
   scene.background = new Color('skyblue');
 
-  const geometry = new BoxBufferGeometry(0.2, 0.2, 0.2);
-  const material = new MeshNormalMaterial();
+  const geometries = testCSG();
+  // console.log('geometries: ', geometries);
 
-  const mesh = new Mesh(geometry, material);
-  scene.add(mesh);
+  const material = new MeshNormalMaterial({ side: DoubleSide });
+
+  const triangle = new Mesh(geometries.triangle, material);
+  triangle.position.set(-1, 1, 0);
+  const triangleOrig = new Mesh(geometries.triangleOrig, material);
+  triangleOrig.position.set(-0.5, 1, 0);
+
+  const square = new Mesh(geometries.square, material);
+  square.position.set(0, 1, 0);
+  const squareOrig = new Mesh(geometries.squareOrig, material);
+  squareOrig.position.set(0.5, 1, 0);
+
+  const plane = new Mesh(geometries.plane, material);
+  plane.position.set(1, 1, 0);
+  const planeOrig = new Mesh(geometries.planeOrig, material);
+  planeOrig.position.set(1.5, 1, 0);
+
+  const box = new Mesh(geometries.box, material);
+  box.position.set(-1, 0, 0);
+  const boxOrig = new Mesh(geometries.boxOrig, material);
+  boxOrig.position.set(-0.5, 0, 0);
+
+  const cylinder = new Mesh(geometries.cylinder, material);
+  cylinder.position.set(0, 0, 0);
+  const cylinderOrig = new Mesh(geometries.cylinderOrig, material);
+  cylinderOrig.position.set(0.5, 0, 0);
+
+  const sphere = new Mesh(geometries.sphere, material);
+  sphere.position.set(-1, -1, 0);
+  const sphereOrig = new Mesh(geometries.sphereOrig, material);
+  sphereOrig.position.set(-0.5, -1, 0);
+
+  // const subTest = new Mesh(geometries.subTest, material);
+  // subTest.position.set(1, -1, 0);
+  // console.log('geometries.subTest: ', geometries.subTest);
+
+  // tri
+  // -0.2, -0.2,  0.2,
+  // 0.2, -0.2,  0.2,
+  // 0.2,  0.2,  0.2,
+
+  // basic
+  // 0.2,  0.2,  0.2,
+  // 0.2,  -0.2,  0.2,
+  // -0.2,  -0.2,  0.2,
+
+  const basic = new Mesh(geometries.basic, material);
+  basic.position.set(1, -1, 0);
+  // console.log(
+  //   'geometries.basic: ',
+  //   geometries.basic.attributes.position.array,
+  // );
+
+  scene.add(
+    triangle,
+    triangleOrig,
+    square,
+    squareOrig,
+    plane,
+    planeOrig,
+    box,
+    boxOrig,
+    cylinder,
+    cylinderOrig,
+    sphere,
+    sphereOrig,
+    basic,
+    // subTest,
+  );
 
   const renderer = new WebGLRenderer({ antialias: true });
   renderer.setSize(container.clientWidth, container.clientHeight);
@@ -39,9 +109,6 @@ function init() {
   controls.enableDamping = true;
 
   renderer.setAnimationLoop(() => {
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
-
     controls.update();
     renderer.render(scene, camera);
   });
